f00558b92e9797bb94f7f5274491d7bbf5d9211e
Artem Z. <artemonych628@gmail.com>
2026-04-14 14:55:40 +0200

    feat(toast): Rendering & Callbacks
    Created rendering logic for Toaster Component and manipulation with it throuth observer pattern using toast functoin.

--- src/components/Toast/Toast.stories.tsx
+++ src/components/Toast/Toast.stories.tsx
@@ -1,19 +1,27 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
-import { Toast } from "./Toast";
+import { Toaster, toast, ToastItem } from "./Toast";
+import { Button } from "../Button";
 
 const meta = {
   argTypes: {},
-  component: Toast,
+  component: Toaster,
   parameters: {
     layout: "centered",
   },
   title: "Components/Toast",
-} satisfies Meta<typeof Toast>;
+} satisfies Meta<typeof Toaster>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Defaul: Story = {
   args: {},
+  render: () => {
+
+    return <div>
+      <Button onClick={() => toast({ title: "Toast DEMO", description: "Demonstration of the toaster component", variant: "info", closable: true })}>Trigger toast</Button>
+      <Toaster position="top-right" />
+    </div>
+  }
 };
--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,5 +1,105 @@
-function Toast() {
-  return <div></div>;
+import React, { useState, useEffect, useCallback } from "react";
+import { createPortal } from "react-dom";
+import { cn } from "../../lib/utils";
+import { X } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion"
+
+type ToastVariant = "success" | "error" | "warning" | "info";
+
+interface ToastData {
+  id: string;
+  title?: string;
+  description: string;
+  variant: ToastVariant;
+  duration?: number;
+  icon?: React.ReactNode;
+  closable?: boolean;
 }
 
-export { Toast };
+let toastCount = 0;
+const observers: Array<(toast: ToastData) => void> = [];
+
+const toast = (data: Omit<ToastData, "id">) => {
+  const newToast = { ...data, id: `tost-item-${++toastCount}` };
+  observers.forEach((clbck) => clbck(newToast));
+};
+
+interface ToasterProps {
+  position?: "top-right" | "bottom-right" | "top-left" | "bottom-left" | "top-center" | "bottom-center";
+  offset?: number;
+}
+
+function Toaster({
+  position = "bottom-right",
+  offset = 20
+}: ToasterProps) {
+  const [toasts, setToasts] = useState<ToastData[]>([]);
+
+  useEffect(() => {
+    const addToast = (t: ToastData) => setToasts((prev) => [...prev, t]);
+    observers.push(addToast);
+    return () => { observers.splice(observers.indexOf(addToast), 1); };
+  }, []);
+
+  const removeToast = useCallback((id: string) => {
+    setToasts((prev) => prev.filter((t) => t.id !== id));
+  }, []);
+
+  const positionClasses = {
+    "top-right": "top-0 right-0 flex-col",
+    "bottom-right": "bottom-0 right-0 flex-col-reverse",
+    "top-left": "top-0 left-0 flex-col",
+    "bottom-left": "bottom-0 left-0 flex-col-reverse",
+    "top-center": "top-0 flex-col mx-auto",
+    "bottom-center": "bottom-0 flex-col-reverse mx-auto"
+  };
+
+  return createPortal(
+    <div
+      className={cn("fixed z-[100] p-4 flex gap-2 pointer-events-none", positionClasses[position])}
+      style={{ padding: offset }}
+    >
+      <AnimatePresence>
+        {toasts.map((t) => (
+          <ToastItem key={t.id} data={t} onRemove={() => removeToast(t.id)} />
+        ))}
+      </AnimatePresence>
+    </div>,
+    document.body
+  );
+}
+
+
+interface ToastItemProps extends React.ComponentProps<"div"> {
+  data: ToastData;
+  onRemove: () => void
+}
+
+const VARIANTS: Record<string, string> = {
+  success: "border-error-border bg-error-surface text-error-text",
+  error: "border-success-border bg-success-surface text-success-text",
+  warining: "",
+  info: ""
+}
+
+function ToastItem({ data, onRemove }: ToastItemProps) {
+  useEffect(() => {
+    const timer = setTimeout(onRemove, data.duration || 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <motion.div initial={{ scale: 0.5, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} exit={{ scale: 0.5, opacity: 0 }} className={cn(
+      "pointer-events-auto w-80 p-4 rounded-lg shadow-lg border bg-surface animate-in slide-in-from-right-5 relative overflow-hidden",
+      VARIANTS[data.variant]
+    )}>
+      <h4 className="font-bold">{data.title}</h4>
+      {data.description}
+      {data.closable && <X role="button" onClick={() => onRemove()} className="hover:text-primary cursor-pointer absolute top-2 right-2" />}
+      <motion.div animate={{ width: 0 }} initial={{ width: "100%" }} transition={{ ease: "linear", duration: (data.duration || 5000) / 1000 }} className="bottom-0 left-0 absolute h-1.5 bg-red-500" />
+    </motion.div>
+  );
+}
+
+export { Toaster, ToastItem, toast }
+export type { ToastData, ToasterProps, ToastItemProps }
